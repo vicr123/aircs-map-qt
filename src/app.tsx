74cd4cfc75d9c154ee397b7fda0b90366f186839
Vrabbers <25323861+Vrabbers@users.noreply.github.com>
2025-07-30 12:05:45 -0300

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,15 @@
+import "./app.css"
+
+export function App() {
+    return (
+        <div class="container">
+            <div class="topBar">
+                <button>Directions</button>
+                <input></input>
+                <img class="logo" src="/src/assets/aircslogo.svg" alt="AirCS Logo"/>
+            </div>
+            <main>
+                I am the rest of the area
+            </main>
+        </div>);
+}
