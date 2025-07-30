cf27c8ac310641eaea99294d4f13b5944c9d2d86
Vrabbers <25323861+Vrabbers@users.noreply.github.com>
2025-07-30 19:20:27 -0300

    Try to organize it

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,7 @@
 import "./app.css"
-import {useEffect, useRef} from "preact/hooks";
-
+import {Map} from "./map.tsx";
 
 export function App() {
-    const obj = useRef<HTMLObjectElement>(null);
-    useEffect(() => {
-        if (obj.current === null)
-            return;
-        const loadEv = () => {
-            if (obj.current === null)
-                return;
-
-            const svg = obj.current.contentDocument!;
-
-            svg.getElementById("ACS")?.addEventListener("click", (e) => {console.log(e)});
-        };
-
-        obj.current.addEventListener("load", loadEv);
-
-        return () => {
-            obj.current?.removeEventListener("load", loadEv);
-        }
-    })
     return (
         <div class="container">
             <div class="topBar">
@@ -29,8 +9,7 @@ export function App() {
                 <input></input>
                 <img class="logo" src="/src/assets/aircslogo.svg" alt="AirCS Logo"/>
             </div>
-            <main class="mapContainer">
-                <object ref={obj} class="map" data="map.svg" type="image/svg+xml" aria-description="AirCS Map"/>
-            </main>
-        </div>);
+            <Map/>
+        </div>
+    );
 }
---
+++ src/map.tsx
@@ -0,0 +1,38 @@
+﻿import {useEffect, useMemo, useRef, useState} from "preact/hooks";
+import type {RefObject} from "preact";
+
+function Svg({ref: obj}: {ref: RefObject<HTMLObjectElement>}) {
+    function loadEv() {
+        const svg = obj.current!.contentDocument!;
+
+        console.log(svg.getElementById("ACS"));
+
+        for (const i of ["ACS", "AAR"]) {
+            svg.getElementById(i)!.addEventListener("click", (e) => {
+                console.log(i);
+            });
+        }
+
+        svg.addEventListener("click", (e) => {console.log(e)});
+    }
+
+    return <object ref={obj} className="map" data="map.svg" type="image/svg+xml" aria-description="AirCS Map"
+                   onLoad={loadEv} onClick={() => console.log("a")}/>;
+}
+
+export function Map() {
+    const obj = useRef<HTMLObjectElement>(null);
+    const svg = useMemo(() => Svg({ref: obj}), []);
+    const [st, setSt] = useState(2);
+
+    useEffect(() => {
+        obj.current!.style.transform = `scale(${st})`;
+    }, [st]);
+
+    return <div className="mapContainer" children={svg} onClickCapture={a} />;
+
+    function a() {
+        console.log(st);
+        return setSt(st + 0.1);
+    }
+}
