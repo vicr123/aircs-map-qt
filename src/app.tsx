5e2d8db806c61da500d4f3b8bbf8fd0f35fc8cac
Vrabbers <25323861+Vrabbers@users.noreply.github.com>
2025-07-30 13:04:51 -0300

    Use SVG object and click event proof of concept

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,27 @@
 import "./app.css"
+import {useEffect, useRef} from "preact/hooks";
+
 
 export function App() {
+    const obj = useRef<HTMLObjectElement>(null);
+    useEffect(() => {
+        if (obj.current === null)
+            return;
+        const loadEv = () => {
+            if (obj.current === null)
+                return;
+
+            const svg = obj.current.contentDocument!;
+
+            svg.getElementById("ACS")?.addEventListener("click", (e) => {console.log(e)});
+        };
+
+        obj.current.addEventListener("load", loadEv);
+
+        return () => {
+            obj.current?.removeEventListener("load", loadEv);
+        }
+    })
     return (
         <div class="container">
             <div class="topBar">
@@ -8,8 +29,8 @@ export function App() {
                 <input></input>
                 <img class="logo" src="/src/assets/aircslogo.svg" alt="AirCS Logo"/>
             </div>
-            <main>
-                I am the rest of the area
+            <main class="mapContainer">
+                <object ref={obj} class="map" data="map.svg" type="image/svg+xml" aria-description="AirCS Map"/>
             </main>
         </div>);
 }
